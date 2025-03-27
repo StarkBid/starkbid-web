443b72188df9e1703f492f82e20b6063e0df37e9
davedumto <ejeredavid2001@gmail.com>
2025-03-27 11:00:20 +0100

    feat: built the you may also like section and added a hover animation similar to the previous one I worked on

--- components/landing-page/trending-nft.tsx
+++ components/landing-page/trending-nft.tsx
@@ -42,7 +42,7 @@ const NFTCard: React.FC<NFTCardProps> = ({
           onClick={() => setIsLiked(!isLiked)}
           className="text-ash hover:text-red-500 transition-colors"
         >
-          {isLiked ? <BsHeartFill className="text-ash" size={20} /> : <BsHeart size={20} />}
+          {isLiked ? <BsHeartFill className="text-red" size={20} /> : <BsHeart size={20} />}
         </button>
       </div>
 
@@ -86,7 +86,7 @@ const TrendingNFTs: React.FC = () => {
   const [activeTab, setActiveTab] = useState('trending');
 
   return (
-    <div className="bg-black text-white p-6 max-w-8xl mx-auto">
+    <div className="bg-black text-white py-6 px-[1.1em] max-w-8xl mx-auto">
       <div className="flex gap-[1em] mb-8">
         <button
           className={`px-2 py-1 ${activeTab === 'trending' ? 'text-white font-bold  text-xl bg-[#1C1D1F] px-4 py-2 rounded-md' : 'text-gray-500  text-xl'}`}
